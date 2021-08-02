28bab7646648c21b4450bdccaf4815d51ffe68d3
Rajni Dua <rajnidua@Rajnis-MBP.hub>
2021-08-02 16:58:06 +0930

    updated CSS JS HTML and screenshots

--- README.md
+++ README.md
@@ -2,6 +2,33 @@
 
 # rajniPortfolio
 
+This is my portfolio project, that shows the work that I have done and is fully responsive.
+
+
+When User clicks on the item in the navigation bar,
+Then it takes you to the appropriate section
+
+When you hover over the project,
+Then it shows you the dtails about the project
+
+When you click on the project name 
+Then it takes you to the deployed app
+
+When you click on the github link,
+then it takes you to the github link for that project
+
+When you click on the CONTACT DETAIL link in the Get In Touch section,
+Then it takes you to conatct details in the footer
+
+When user fills out form with his name ,email id and message,
+Then I recieve an email in the email.
+
+
+## Screenshots
+![](./assets/images/ScreenShot1.png)
+![](./assets/images/ScreenShot2.png)
+
+
 
 ## Lisence & copyright
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,82 @@
+var animatedFrontNames= document.getElementById("animatedFrontNames");
+var i = 0;
+var txt = 'Browser Based Technologies : HTML CSS JAVASCRIPT JQUERY';
+var speed = 70;
+
+typeWriter();
+
+
+
+
 window.onbeforeunload = () => {
     for(const form of document.getElementsByTagName('form')) {
       form.reset();
     }
   }
+
+
+  
+
+
+
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("animatedFrontNames").innerHTML += txt.charAt(i);
+    
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+  else{
+    i = 0;
+ txt = 'SERVER SIDE DEVELOPMENT : Node JS';
+
+    typeWriterBackend();
+  }
+}
+
+
+function typeWriterBackend() {
+  if (i < txt.length) {
+    document.getElementById("animatedBackNames").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriterBackend, speed);
+  }
+  else{
+    i = 0;
+ txt = 'API INTERACTION : API JSON AJAX';
+
+    typeWriterApi();
+    
+
+  }
+  
+}
+
+
+function typeWriterApi() {
+  if (i < txt.length) {
+    document.getElementById("animatedApiNames").innerHTML += txt.charAt(i);
+    
+    i++;
+    setTimeout(typeWriterApi, speed);
+  }
+  
+}
+
+
+/*function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
+} */
