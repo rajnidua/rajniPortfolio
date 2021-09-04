f8df40e953f226fccfc7d72545164f1bde396f39
Rajni Dua <rajni.dua14@gmail.com>
2021-09-04 20:02:11 +0930

    updated Cv

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,48 @@
-var animatedFrontNames= document.getElementById("animatedFrontNames");
+var animatedFrontNames = document.getElementById("animatedFrontNames");
 var i = 0;
-var txt = 'Browser Based Technologies : HTML CSS JAVASCRIPT JQUERY';
+var txt = "Browser Based Technologies : HTML CSS JAVASCRIPT JQUERY";
 var speed = 70;
 
 typeWriter();
 
-
-
-
 window.onbeforeunload = () => {
-    for(const form of document.getElementsByTagName('form')) {
-      form.reset();
-    }
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
   }
-
-
-  
-
-
-
+};
 
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById("animatedFrontNames").innerHTML += txt.charAt(i);
-    
+
     i++;
     setTimeout(typeWriter, speed);
-  }
-  else{
+  } else {
     i = 0;
- txt = 'SERVER SIDE DEVELOPMENT : Node JS';
+    txt = "SERVER SIDE DEVELOPMENT : Node JS Express JS MYSQL2";
 
     typeWriterBackend();
   }
 }
-
 
 function typeWriterBackend() {
   if (i < txt.length) {
     document.getElementById("animatedBackNames").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriterBackend, speed);
-  }
-  else{
+  } else {
     i = 0;
- txt = 'API INTERACTION : API JSON AJAX';
+    txt = "API INTERACTION : API JSON AJAX";
 
     typeWriterApi();
-    
-
   }
-  
 }
-
 
 function typeWriterApi() {
   if (i < txt.length) {
     document.getElementById("animatedApiNames").innerHTML += txt.charAt(i);
-    
+
     i++;
     setTimeout(typeWriterApi, speed);
   }
-  
 }
-
-
-/*function setTime() {
-  // Sets interval in variable
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
-
-    if(secondsLeft === 0) {
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
-      // Calls function to create and append image
-      sendMessage();
-    }
-
-  }, 1000);
-} */
